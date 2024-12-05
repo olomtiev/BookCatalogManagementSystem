83fd3e063754217d715bd02b21bec13657d53409
Oleksandr Lomtiev <132142322+olomtiev@users.noreply.github.com>
2024-12-05 16:59:45 +0200

    Update Program.cs

--- BookCatalog/Server/Program.cs
+++ BookCatalog/Server/Program.cs
@@ -10,13 +10,12 @@ namespace BookCatalog
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Добавление политики CORS
             builder.Services.AddCors(options =>
             {
                 options.AddPolicy("AllowAll", builder =>
-                    builder.AllowAnyOrigin()   // Разрешает любой источник
-                           .AllowAnyMethod()   // Разрешает любые HTTP методы (GET, POST и т.д.)
-                           .AllowAnyHeader()); // Разрешает любые заголовки
+                    builder.AllowAnyOrigin()  
+                           .AllowAnyMethod()   
+                           .AllowAnyHeader()); 
             });
 
             builder.Services.AddMemoryCache();
@@ -24,16 +23,13 @@ namespace BookCatalog
             builder.Services.AddInMemoryRateLimiting();
             builder.Services.AddSingleton<IRateLimitConfiguration, RateLimitConfiguration>();
 
-            // Добавление репозитория
             builder.Services.AddSingleton<IBookRepository, InMemoryBookRepository>();
 
-            // Добавление сервисов для контроллеров и страниц
             builder.Services.AddControllersWithViews();
             builder.Services.AddRazorPages();
 
             var app = builder.Build();
 
-            // Применение CORS политики
             app.UseCors("AllowAll");
              
             if (app.Environment.IsDevelopment())
